cbf7c41fa7f0b003386d3e859e7269cf954728ad
Joseph Jojo <josephjojojohn@gmail.com>
2020-04-03 03:41:55 +0530

    Routing Definition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogListComponent } from './blog/blog-list/blog-list.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { BlogNewComponent } from './admin/blog-new/blog-new.component';
+import { LoginComponent } from './admin/login/login.component';
+import { BlogEditComponent } from './admin/blog-edit/blog-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlogListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: DashboardComponent },
+  { path: 'admin/blog/new', component: BlogNewComponent },
+  { path: 'admin/blog/edit/:id', component: BlogEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
