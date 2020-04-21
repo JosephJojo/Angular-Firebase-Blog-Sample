d82a073d306761c277b521604bbf11b9c74efa2a
Joseph Jojo <josephjojojohn@gmail.com>
2020-04-22 02:57:02 +0530

    Authentication Module
    - Google SignIn
    - Navigation and comments change based on authentication

--- src/app/blog/blog-view/blog-view.component.ts
+++ src/app/blog/blog-view/blog-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from '../../shared/blog.service';
 import { Blog } from '../../shared/blog.model';
+import { AuthService } from '../../shared/auth.service';
 
 @Component({
   selector: 'app-blog-view',
@@ -12,7 +13,7 @@ export class BlogViewComponent implements OnInit {
 
   blogPost: Blog;
 
-  constructor(private route: ActivatedRoute, private blogService: BlogService) { }
+  constructor(private route: ActivatedRoute, private blogService: BlogService, public auth: AuthService) { }
 
   loadPost(): void {
     const id = this.route.snapshot.paramMap.get('id');
