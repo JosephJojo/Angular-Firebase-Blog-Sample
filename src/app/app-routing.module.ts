893de2a819cba57c14dae30fede4bba98435aa09
Joseph Jojo <josephjojojohn@gmail.com>
2020-04-22 04:23:11 +0530

    Authguard and user profile picture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,15 @@ import { LoginComponent } from './admin/login/login.component';
 import { BlogEditComponent } from './admin/blog-edit/blog-edit.component';
 import { BlogViewComponent } from './blog/blog-view/blog-view.component';
 
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   { path: '', component: BlogListComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: DashboardComponent },
-  { path: 'admin/blog/new', component: BlogNewComponent },
+  { path: 'admin', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'admin/blog/new', component: BlogNewComponent, canActivate: [AuthGuard] },
   { path: 'blog/view/:id', component: BlogViewComponent },
-  { path: 'admin/blog/edit/:id', component: BlogEditComponent }
+  { path: 'admin/blog/edit/:id', component: BlogEditComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
